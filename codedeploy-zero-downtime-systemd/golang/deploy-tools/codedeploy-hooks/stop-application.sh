0f09dc81733a31c4e2455047a7f03f40bca1eb6a
lambrospetrou <lambrospetrou@gmail.com>
2020-10-25 01:02:23 +0100

    fix application stop script

--- codedeploy-zero-downtime-systemd/golang/deploy-tools/codedeploy-hooks/stop-application.sh
+++ codedeploy-zero-downtime-systemd/golang/deploy-tools/codedeploy-hooks/stop-application.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -ex
+set -x
 
 echo -e "Stopping the application..."
 
-UNIT_EXISTS=$( systemctl list-units | grep web )
+UNIT_EXISTS=$( systemctl list-units | grep web.service )
 [[ ! -z "$UNIT_EXISTS" ]] && systemctl stop web.service
 
 echo -e "Stopped the application."
